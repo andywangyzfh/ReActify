24934cd222d9c20d32a6aa5281a2d590c24f1a47
Kevincklhhh <Kevin_ckl@outlook.com>
2023-03-31 20:26:08 -0400

    ckend

--- query.py
+++ query.py
@@ -1,17 +1,46 @@
 import openai
 import os
-import IPython
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+import sys
+from io import StringIO
+from langchain.chat_models import ChatOpenAI
 
+class Query:
+    showProcess = 0
+    temperature = 0
+    def __init__(self):
+        load_dotenv()
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+        os.environ["SERPAPI_API_KEY"] = os.getenv("SERPAPI_API_KEY")
 
-def setToken():
-    load_dotenv()
+    def setModel(self,showProcess, temperature):
+        self.showProcess = showProcess
+        self.temperature = temperature
 
-    # API configuration
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
-    # for LangChain
-    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-    os.environ["SERPAPI_API_KEY"] = os.getenv("SERPAPI_API_KEY")
-
+    def getResponse(self,prompt):
+        chat = ChatOpenAI(temperature=self.temperature)
+    # Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
+        llm = OpenAI(temperature=self.temperature)
+        tools = load_tools(["serpapi", "llm-math"], llm=llm)
+    # Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+        agent = initialize_agent(tools, chat, agent="chat-zero-shot-react-description", verbose=True)
+    # Now let's test it out!
+        output = StringIO()
+        sys.stdout = output
+        final_result = agent.run(prompt)
+        sys.stdout = sys.__stdout__
+    # Retrieve the output from the StringIO object as a string
+        react_process = output.getvalue()
+        if(self.showProcess):
+            return react_process
+        else:
+            return final_result
+if __name__ == "__main__":
+    #for testing purposes
+    myquery = Query()
+    myquery.setModel(1,0)
+    print(myquery.getResponse("In the city where the CEO of Tesla was born, what is the average price of houses?"))
